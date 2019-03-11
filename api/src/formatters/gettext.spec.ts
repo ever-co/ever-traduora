856bb216e8f4a410164c0822f1bfa8e048978d03
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2019-03-12 00:07:06 +0100

    Add basic formatters for gettext

---
+++ api/src/formatters/gettext.spec.ts
@@ -0,0 +1,14 @@
+import { loadFixture, simpleFormatFixture } from './fixtures';
+import { gettextExporter, gettextParser } from './gettext';
+
+test('should parse gettext files', async () => {
+  const input = loadFixture('simple.po');
+  const result = await gettextParser(input);
+  expect(result).toEqual(simpleFormatFixture);
+});
+
+test('should export gettext files', async () => {
+  const result = await gettextExporter({ ...simpleFormatFixture, iso: 'de_DE' });
+  const expected = loadFixture('simple.po');
+  expect(result).toEqual(expected);
+});
---
+++ api/src/formatters/gettext.ts
@@ -0,0 +1,57 @@
+import { po } from 'gettext-parser';
+import { Exporter, IntermediateTranslationFormat, Parser } from '../domain/formatters';
+
+const streamAsPromise = stream => {
+  const result = [];
+
+  stream.on('data', data => {
+    result.push(data);
+  });
+
+  return new Promise((resolve, reject) => {
+    stream.on('finish', resolve);
+    stream.on('error', reject);
+  }).then(() => result);
+};
+
+export const gettextParser: Parser = async (data: string) => {
+  const res = po.parse(data);
+  const payload = res.translations[''];
+
+  const translations = [];
+
+  if (payload !== undefined) {
+    for (const key of Object.keys(payload)) {
+      if (key === '') {
+        continue;
+      }
+      const item = payload[key];
+      translations.push({ term: item.msgid, translation: item.msgstr.join('') });
+    }
+  }
+
+  return {
+    translations,
+  };
+};
+
+export const gettextExporter: Exporter = async (data: IntermediateTranslationFormat) => {
+  const asObj = data.translations.reduce((acc, item) => ({ ...acc, [item.term]: { msgid: item.term, msgstr: item.translation } }), {});
+  const out = {
+    charset: 'utf-8',
+    headers: {
+      'content-type': 'text/plain; charset=utf-8',
+      'Content-Transfer-Encoding': '8bit\n',
+      'MIME-Version': '1.0\n',
+    },
+    translations: {
+      '': asObj,
+    },
+  };
+  if (data.iso) {
+    // tslint:disable-next-line: no-string-literal
+    out.headers['Language'] = `${data.iso}\n`;
+  }
+  const res = po.compile(out).toString();
+  return res;
+};
