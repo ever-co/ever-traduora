589b40b74a4766e5516018c870c7466cf8eb4f20
Botond Magyarosi <magyarosibotond@gmail.com>
2019-03-15 01:05:02 +0200

    Fix tests & add changelog

--- api/src/formatters/strings.spec.ts
+++ api/src/formatters/strings.spec.ts
@@ -1,14 +1,14 @@
-import { loadFixture, simpleFormatFixture } from "./fixtures";
-import { stringsParser, stringsExporter } from "./strings";
+import { loadFixture, simpleFormatFixture } from './fixtures';
+import { stringsParser, stringsExporter } from './strings';
 
 test('should parse strings files', async () => {
-    const input = loadFixture('simple.strings');
-    const result = await stringsParser(input);
-    expect(result).toEqual(simpleFormatFixture);
+  const input = loadFixture('simple.strings');
+  const result = await stringsParser(input);
+  expect(result).toEqual(simpleFormatFixture);
 });
 
 test('should export strings files', async () => {
-    const result = await stringsExporter(simpleFormatFixture);
-    const expected = loadFixture('simple.strings');
-    expect(result).toEqual(expected);
-})
+  const result = await stringsExporter(simpleFormatFixture);
+  const expected = loadFixture('simple.strings');
+  expect(result).toEqual(expected);
+});
--- api/src/formatters/strings.ts
+++ api/src/formatters/strings.ts
@@ -1,10 +1,10 @@
-import { Parser, Exporter, IntermediateTranslationFormat } from "domain/formatters";
+import { Parser, Exporter, IntermediateTranslationFormat } from 'domain/formatters';
 import * as strings from 'strings-file';
 
 export const stringsParser: Parser = async (data: string) => {
   const parsed = strings.parse(data, false);
   const translations = [];
-  
+
   for (const term of Object.keys(parsed)) {
     const translation = parsed[term] || '';
     translations.push({
@@ -12,13 +12,13 @@ export const stringsParser: Parser = async (data: string) => {
       translation,
     });
   }
-  
+
   return {
     translations,
-  }
+  };
 };
 
 export const stringsExporter: Exporter = async (data: IntermediateTranslationFormat) => {
   const out = data.translations.reduce((acc, obj) => ({ ...acc, [obj.term]: obj.translation }), {});
-  return strings.compile(out).replace(/^\s*$(?:\r\n?|\n)/gm,"")
+  return strings.compile(out).replace(/^\s*$(?:\r\n?|\n)/gm, '');
 };
