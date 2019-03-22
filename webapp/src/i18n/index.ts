c884b9c65161f9acbe6e7f30c60565dc26b1b649
Anthony Najjar Simon <anthonynsimon@users.noreply.github.com>
2019-03-22 17:51:01 +0100

    Fmt

--- webapp/src/i18n/index.ts
+++ webapp/src/i18n/index.ts
@@ -1,73 +1,70 @@
-import { Observable, BehaviorSubject, Subject, of } from "rxjs";
+import { Observable, BehaviorSubject, Subject, of } from 'rxjs';
 import { map, switchMap, tap } from 'rxjs/operators';
 
 interface TranslationStore {
-    load(locale: string): Observable<LocaleTranslations>;
-    availableLocales(): Observable<string>;
+  load(locale: string): Observable<LocaleTranslations>;
+  availableLocales(): Observable<string>;
 }
 
 interface TraduoraConfig {
-    apiKey: string;
-    apiSecret: string;
+  apiKey: string;
+  apiSecret: string;
 }
 
 class Traduora implements TranslationStore {
-    constructor(private config: TraduoraConfig) {}
+  constructor(private config: TraduoraConfig) {}
 
-    load(locale: string): Observable<{ [term: string]: string; }> {
-        throw new Error("Method not implemented.");
-    }
+  load(locale: string): Observable<{ [term: string]: string }> {
+    throw new Error('Method not implemented.');
+  }
 
-    availableLocales(): Observable<string> {
-        throw new Error("Method not implemented.");
-    }
+  availableLocales(): Observable<string> {
+    throw new Error('Method not implemented.');
+  }
 }
 
 interface TranslationsConfig {
-    defaultLocale: string;
-    store: TranslationStore;
+  defaultLocale: string;
+  store: TranslationStore;
 }
 
 type LocaleTranslations = { [term: string]: string };
 
 class Translations {
-    private cache: { [locale: string]: Observable<LocaleTranslations> }
-    private locale: Subject<string>;
-    private store: TranslationStore;
+  private cache: { [locale: string]: Observable<LocaleTranslations> };
+  private locale: Subject<string>;
+  private store: TranslationStore;
 
-    constructor(config: TranslationsConfig) {
-        this.locale = new BehaviorSubject(config.defaultLocale);
-        this.store = config.store;
-    }
+  constructor(config: TranslationsConfig) {
+    this.locale = new BehaviorSubject(config.defaultLocale);
+    this.store = config.store;
+  }
 
-    availableLocales(): Observable<string> {
-        return this.store.availableLocales();
-    }
+  availableLocales(): Observable<string> {
+    return this.store.availableLocales();
+  }
 
-    getCurrentLocale(): Observable<string> {
-        return this.locale.asObservable();
-    }
+  getCurrentLocale(): Observable<string> {
+    return this.locale.asObservable();
+  }
 
-    setCurrentLocale(locale: string): void {
-        this.locale.next(locale);
-    }
+  setCurrentLocale(locale: string): void {
+    this.locale.next(locale);
+  }
 
-    get(term: string, defaultValue: string): Observable<string> {
-        return this.locale.pipe(
-            switchMap(locale => {
-                let cached = this.cache[locale];
-                if (!cached) {
-                    // Store subject in cache to avoid further calls to get to also trigger the load
-                    const sub = new Subject<LocaleTranslations>();
-                    this.cache[locale] = sub.asObservable();
-                    return this.store.load(locale).pipe(
-                        tap(result => sub.next(result))
-                    );
-                }
-                return cached;
-
-            }),
-            map(translations => translations[term] || defaultValue)
-        )
-    }
+  get(term: string, defaultValue: string): Observable<string> {
+    return this.locale.pipe(
+      switchMap(locale => {
+        let cached = this.cache[locale];
+        if (!cached) {
+          // Store subject in cache to avoid further calls to get to also trigger the load
+          const sub = new Subject<LocaleTranslations>();
+          this.cache[locale] = sub.asObservable();
+          return this.store.load(locale).pipe(tap(result => sub.next(result)));
+        }
+        return cached;
+      }),
+      map(translations => translations[term] || defaultValue),
+    );
+  }
 }
