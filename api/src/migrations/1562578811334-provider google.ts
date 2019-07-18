b823c8706e05c62682711078b6464d99b0892140
Victor <victor@hackages.io>
2019-07-18 18:34:09 +0200

    fix(typeorm): remove bad migration lines

--- api/src/migrations/1562578811334-provider google.ts
+++ api/src/migrations/1562578811334-provider google.ts
@@ -2,18 +2,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class providerGoogle1562578811334 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query('ALTER TABLE `project` CHANGE `description` `description` varchar(255) NULL');
     await queryRunner.query('ALTER TABLE `user` CHANGE `encryptedPassword` `encryptedPassword` binary(60) NULL');
-    await queryRunner.query('ALTER TABLE `translation` DROP COLUMN `value`');
-    await queryRunner.query('ALTER TABLE `translation` ADD `value` text NOT NULL');
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query('ALTER TABLE `translation` DROP COLUMN `value`');
-    await queryRunner.query('ALTER TABLE `translation` ADD `value` mediumtext CHARACTER SET "utf8mb4" COLLATE "utf8mb4_unicode_ci" NOT NULL');
     await queryRunner.query('ALTER TABLE `user` CHANGE `encryptedPassword` `encryptedPassword` binary(60) NOT NULL');
-    await queryRunner.query(
-      'ALTER TABLE `project` CHANGE `description` `description` varchar(255) CHARACTER SET "utf8mb4" COLLATE "utf8mb4_unicode_ci" NULL',
-    );
   }
 }
