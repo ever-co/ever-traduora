b46d467fc0b19fef77da93a39e782b8c763f6259
Anthony Najjar Simon <anthonynsimon@users.noreply.github.com>
2019-12-07 12:32:56 +0100

    Cleanup imports

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,8 @@
 import { MorganMiddleware } from '@nest-middlewares/morgan';
-import { HttpModule, INestApplication, MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { HttpModule, MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
 import { PassportModule } from '@nestjs/passport';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { renderFile } from 'ejs';
 import { config } from './config';
@@ -13,32 +13,32 @@ import { ImportController } from './controllers/import.controller';
 import IndexController from './controllers/index.controller';
 import LocaleController from './controllers/locale.controller';
 import ProjectClientController from './controllers/project-client.controller';
+import ProjectInviteController from './controllers/project-invite.controller';
 import ProjectPlanController from './controllers/project-plan.controller';
 import ProjectUserController from './controllers/project-user.controller';
-import ProjectInviteController from './controllers/project-invite.controller';
 import ProjectController from './controllers/project.controller';
+import TagController from './controllers/tags.controller';
 import TermController from './controllers/term.controller';
 import TranslationController from './controllers/translation.controller';
 import UserController from './controllers/user.controller';
+import { Invite } from './entity/invite.entity';
 import { Locale } from './entity/locale.entity';
 import { Plan } from './entity/plan.entity';
 import { ProjectClient } from './entity/project-client.entity';
 import { ProjectLocale } from './entity/project-locale.entity';
 import { ProjectUser } from './entity/project-user.entity';
 import { Project } from './entity/project.entity';
+import { Tag } from './entity/tag.entity';
 import { Term } from './entity/term.entity';
+import { TranslationTag } from './entity/translation-tag.entity';
 import { Translation } from './entity/translation.entity';
 import { User } from './entity/user.entity';
-import { Invite } from './entity/invite.entity';
 import { CustomExceptionFilter } from './filters/exception.filter';
 import { AuthService } from './services/auth.service';
 import AuthorizationService from './services/authorization.service';
 import { JwtStrategy } from './services/jwt.strategy';
 import MailService from './services/mail.service';
 import { UserService } from './services/user.service';
-import { Tag } from './entity/tag.entity';
-import { TranslationTag } from './entity/translation-tag.entity';
-import TagController from './controllers/tags.controller';
 
 @Module({
   imports: [
