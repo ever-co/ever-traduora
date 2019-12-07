f91cdcebf3e887d0d98724978e6fa8fb1a2537bf
Anthony Najjar Simon <anthonynsimon@users.noreply.github.com>
2019-12-07 20:48:15 +0100

    Fix test names

--- api/src/formatters/android-xml-spec.ts
+++ api/src/formatters/android-xml-spec.ts
@@ -1,13 +1,13 @@
 import { loadFixture, simpleFormatFixture } from './fixtures';
 import { androidXmlExporter, androidXmlParser } from './android-xml';
 
-test('should parse gettext files', async () => {
+test('should parse android resources files', async () => {
   const input = loadFixture('simple-android.xml');
   const result = await androidXmlParser(input);
   expect(result).toEqual(simpleFormatFixture);
 });
 
-test('should export gettext files', async () => {
+test('should export android resources files', async () => {
   const result = await androidXmlExporter({ ...simpleFormatFixture, iso: 'de_DE' });
   const expected = loadFixture('simple-android.xml');
   expect(result).toEqual(expected);
