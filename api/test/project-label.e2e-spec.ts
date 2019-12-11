141758fd7bb2d46cbd91076bee25e3b5383b3df3
Anthony Najjar Simon <anthonynsimon@users.noreply.github.com>
2019-12-11 23:40:14 +0100

    Add propagation tests

--- api/test/project-label.e2e-spec.ts
+++ api/test/project-label.e2e-spec.ts
@@ -3,6 +3,7 @@ import * as request from 'supertest';
 import { Connection } from 'typeorm';
 import './util';
 import { createAndMigrateApp, createTestProject, signupTestUser, TestingProject, TestingUser } from './util';
+import { jsonFlatExporter } from '../src/formatters/jsonflat';
 
 describe('ProjectLabelController (e2e)', () => {
   let app: INestApplication;
@@ -189,13 +190,213 @@ describe('ProjectLabelController (e2e)', () => {
       .expect(201);
 
     await request(app.getHttpServer())
-      .get(`/api/v1/projects/${testProject.id}/terms?includeLabels=true`)
+      .get(`/api/v1/projects/${testProject.id}/terms`)
       .set('Authorization', `Bearer ${testingUser.accessToken}`)
       .expect(200)
       .expect(res => {
         expect(res.body.data[0].labels).toHaveLength(1);
         expect(res.body.data[0].labels[0].value).toEqual('todo');
         expect(res.body.data[0].labels[0].color).toEqual('#202020');
+      });
+  });
+
+  it('should propagate term labels to translations whenever a term, translation or project locale is created', async () => {
+    let labelId: string;
+    let labelId2: string;
+    let termId: string;
+    let termId2: string;
+
+    await request(app.getHttpServer())
+      .post(`/api/v1/projects/${testProject.id}/labels`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .send({
+        value: 'todo',
+        color: '#202020',
+      })
+      .expect(201)
+      .expect(res => (labelId = res.body.data.id));
+
+    await request(app.getHttpServer())
+      .post(`/api/v1/projects/${testProject.id}/labels`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .send({
+        value: 'done',
+        color: '#808080',
+      })
+      .expect(201)
+      .expect(res => (labelId2 = res.body.data.id));
+
+    await request(app.getHttpServer())
+      .post(`/api/v1/projects/${testProject.id}/terms`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .send({
+        value: 'term.one',
+      })
+      .expect(201)
+      .expect(res => (termId = res.body.data.id));
+
+    await request(app.getHttpServer())
+      .post(`/api/v1/projects/${testProject.id}/terms`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .send({
+        value: 'term.two',
+      })
+      .expect(201)
+      .expect(res => (termId2 = res.body.data.id));
+
+    // Label term one
+    await request(app.getHttpServer())
+      .post(`/api/v1/projects/${testProject.id}/labels/${labelId}/terms/${termId}`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .expect(201);
+
+    // Term should have label
+    await request(app.getHttpServer())
+      .get(`/api/v1/projects/${testProject.id}/terms`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data[0].labels).toHaveLength(1);
+        expect(res.body.data[0].labels[0].value).toEqual('todo');
+        expect(res.body.data[0].labels[0].color).toEqual('#202020');
+      });
+
+    await request(app.getHttpServer())
+      .post(`/api/v1/projects/${testProject.id}/translations`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .send({
+        code: 'de_DE',
+      })
+      .expect(201);
+
+    await request(app.getHttpServer())
+      .patch(`/api/v1/projects/${testProject.id}/translations/de_DE`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .send({
+        termId,
+        value: 'eins',
+      })
+      .expect(200);
+
+    // Translation should have label from term copied over
+    await request(app.getHttpServer())
+      .get(`/api/v1/projects/${testProject.id}/translations/de_DE`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data).toHaveLength(2);
+        expect(res.body.data[0]).toHaveExactProperties(['termId', 'value', 'labels', 'date']);
+        const translation = res.body.data.find(v => v.termId === termId);
+        expect(translation.value).toEqual('eins');
+        expect(translation.labels).toHaveLength(1);
+        expect(translation.labels[0].value).toEqual('todo');
+        expect(translation.labels[0].color).toEqual('#202020');
+
+        const translationOther = res.body.data.find(v => v.termId === termId2);
+        expect(translationOther.value).toEqual('');
+        expect(translationOther.labels).toHaveLength(0);
+      });
+
+    // Unlabel translation
+    await request(app.getHttpServer())
+      .delete(`/api/v1/projects/${testProject.id}/labels/${labelId}/terms/${termId}/translations/de_DE`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .expect(204);
+
+    await request(app.getHttpServer())
+      .get(`/api/v1/projects/${testProject.id}/translations/de_DE`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data).toHaveLength(2);
+        expect(res.body.data[0]).toHaveExactProperties(['termId', 'value', 'labels', 'date']);
+        const translation = res.body.data.find(v => v.termId === termId);
+        expect(translation.termId).toEqual(termId);
+        expect(translation.value).toEqual('eins');
+      });
+
+    // Try to import
+    const fileToImport = (await jsonFlatExporter({
+      translations: [{ term: 'term.one', translation: 'Hello' }, { term: 'term.two', translation: 'Goodbye' }],
+    })) as string;
+
+    await request(app.getHttpServer())
+      .post(`/api/v1/projects/${testProject.id}/imports?locale=de_DE&format=jsonflat`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .attach('file', Buffer.from(fileToImport), 'file')
+      .expect(200);
+
+    // Translation should have label from term copied over
+    await request(app.getHttpServer())
+      .get(`/api/v1/projects/${testProject.id}/translations/de_DE`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data).toHaveLength(2);
+        expect(res.body.data[0]).toHaveExactProperties(['termId', 'value', 'labels', 'date']);
+        const translation = res.body.data.find(v => v.termId === termId);
+        expect(translation.value).toEqual('Hello');
+        expect(translation.labels).toHaveLength(1);
+        expect(translation.labels[0].value).toEqual('todo');
+        expect(translation.labels[0].color).toEqual('#202020');
+
+        const translationOther = res.body.data.find(v => v.termId === termId2);
+        expect(translationOther.value).toEqual('Goodbye');
+        expect(translationOther.labels).toHaveLength(0);
+      });
+
+    // Label term two
+    await request(app.getHttpServer())
+      .post(`/api/v1/projects/${testProject.id}/labels/${labelId2}/terms/${termId2}`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .expect(201);
+
+    await request(app.getHttpServer())
+      .post(`/api/v1/projects/${testProject.id}/imports?locale=nb_NO&format=jsonflat`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .attach('file', Buffer.from(fileToImport), 'file')
+      .expect(200);
+
+    // Translation should have label from term copied over
+    await request(app.getHttpServer())
+      .get(`/api/v1/projects/${testProject.id}/translations/de_DE`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data).toHaveLength(2);
+        expect(res.body.data[0]).toHaveExactProperties(['termId', 'value', 'labels', 'date']);
+        const translation = res.body.data.find(v => v.termId === termId);
+        expect(translation.value).toEqual('Hello');
+        expect(translation.labels).toHaveLength(1);
+        expect(translation.labels[0].value).toEqual('todo');
+        expect(translation.labels[0].color).toEqual('#202020');
+
+        const translationOther = res.body.data.find(v => v.termId === termId2);
+        expect(translationOther.value).toEqual('Goodbye');
+        expect(translationOther.labels).toHaveLength(1);
+        expect(translationOther.labels[0].value).toEqual('done');
+        expect(translationOther.labels[0].color).toEqual('#808080');
+      });
+
+    await request(app.getHttpServer())
+      .get(`/api/v1/projects/${testProject.id}/translations/nb_NO`)
+      .set('Authorization', `Bearer ${testingUser.accessToken}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data).toHaveLength(2);
+        expect(res.body.data[0]).toHaveExactProperties(['termId', 'value', 'labels', 'date']);
+
+        const translation = res.body.data.find(v => v.termId === termId);
+        expect(translation.value).toEqual('Hello');
+        expect(translation.labels).toHaveLength(1);
+        expect(translation.labels[0].value).toEqual('todo');
+        expect(translation.labels[0].color).toEqual('#202020');
+
+        const translationOther = res.body.data.find(v => v.termId === termId2);
+        expect(translationOther.value).toEqual('Goodbye');
+        expect(translationOther.labels).toHaveLength(1);
+        expect(translationOther.labels[0].value).toEqual('done');
+        expect(translationOther.labels[0].color).toEqual('#808080');
       });
   });
 
@@ -243,7 +444,7 @@ describe('ProjectLabelController (e2e)', () => {
       .expect(201);
 
     await request(app.getHttpServer())
-      .get(`/api/v1/projects/${testProject.id}/terms?includeLabels=true`)
+      .get(`/api/v1/projects/${testProject.id}/terms`)
       .set('Authorization', `Bearer ${testingUser.accessToken}`)
       .expect(200)
       .expect(res => {
@@ -264,7 +465,7 @@ describe('ProjectLabelController (e2e)', () => {
       .expect(204);
 
     await request(app.getHttpServer())
-      .get(`/api/v1/projects/${testProject.id}/terms?includeLabels=true`)
+      .get(`/api/v1/projects/${testProject.id}/terms`)
       .set('Authorization', `Bearer ${testingUser.accessToken}`)
       .expect(200)
       .expect(res => {
