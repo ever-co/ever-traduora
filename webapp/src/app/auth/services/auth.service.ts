8f15987387c34ed158d77b6ce90ebeb2f2bb2d56
Anthony Najjar Simon <anthonynsimon@users.noreply.github.com>
2020-04-06 17:29:45 +0200

    Disable long line lint

--- webapp/src/app/auth/services/auth.service.ts
+++ webapp/src/app/auth/services/auth.service.ts
@@ -50,7 +50,7 @@ export class AuthService {
 
   redirectWithProvider(provider: Provider) {
     window.open(
-      `${provider.url}?redirect_uri=${provider.redirectUrl}&client_id=${provider.clientId}&scope=email profile openid&access_type=offline&prompt=select_account&response_type=code`,
+      `${provider.url}?redirect_uri=${provider.redirectUrl}&client_id=${provider.clientId}&scope=email profile openid&access_type=offline&prompt=select_account&response_type=code`, // tslint:disable-line max-line-length
       '_self',
     );
   }
