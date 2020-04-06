8d7c5d79b6e7314b1163580cbca393bd8aefe843
Anthony Najjar Simon <anthonynsimon@users.noreply.github.com>
2020-04-06 17:27:56 +0200

    Upgrade dependencies and apply fixes

--- api/src/errors/index.ts
+++ api/src/errors/index.ts
@@ -1,19 +1,18 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
-import { createHttpExceptionBody } from '@nestjs/common/utils/http-exception-body.util';
 
 export class TooManyRequestsException extends HttpException {
   constructor(message?: string | object | any, error: string = 'TooManyRequests') {
-    super(createHttpExceptionBody(message, error, HttpStatus.TOO_MANY_REQUESTS), HttpStatus.TOO_MANY_REQUESTS);
+    super(HttpException.createBody(message, error, HttpStatus.TOO_MANY_REQUESTS), HttpStatus.TOO_MANY_REQUESTS);
   }
 }
 export class ConflictException extends HttpException {
   constructor(message?: string | object | any, error: string = 'AlreadyExists') {
-    super(createHttpExceptionBody(message, error, HttpStatus.CONFLICT), HttpStatus.CONFLICT);
+    super(HttpException.createBody(message, error, HttpStatus.CONFLICT), HttpStatus.CONFLICT);
   }
 }
 
 export class PaymentRequiredException extends HttpException {
   constructor(message?: string | object | any, error: string = 'PaymentRequired') {
-    super(createHttpExceptionBody(message, error, HttpStatus.PAYMENT_REQUIRED), HttpStatus.PAYMENT_REQUIRED);
+    super(HttpException.createBody(message, error, HttpStatus.PAYMENT_REQUIRED), HttpStatus.PAYMENT_REQUIRED);
   }
 }
