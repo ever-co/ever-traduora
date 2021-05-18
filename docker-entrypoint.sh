7d5b83db11c001365765db3055934184b919de43
Antonio Falzarano <ynuyasha@users.noreply.github.com>
2021-05-18 14:33:36 +0200

    Fix docker secret support
    this version works on /bin/sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,33 +1,50 @@
 #!/bin/sh
 
-# usage: file_env VAR [DEFAULT]
-#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
-# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
-#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
-file_env() {
-	local var="$1"
-	local fileVar="${var}_FILE"
-	local def="${2:-}"
-	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
-		echo "Both $var and $fileVar are set (but are exclusive)"
-    exit 1
-	fi
-	local val="$def"
-	if [ "${!var:-}" ]; then
-		val="${!var}"
-	elif [ "${!fileVar:-}" ]; then
-		val="$(< "${!fileVar}")"
-	fi
-	export "$var"="$val"
-	unset "$fileVar"
+set -e
+
+#Use docker secrets
+: ${ENV_SECRETS_DIR:=/run/secrets}
+
+env_secret_debug() {
+    if [ ! -z "$ENV_SECRETS_DEBUG" ]; then
+        echo -e "\033[1m$@\033[0m"
+    fi
 }
 
-docker_setup_env() {
-  # Initialize values that might be stored in a file
-	file_env 'TR_SECRET'
-	file_env 'TR_DB_USER'
-	file_env 'TR_DB_PASSWORD'
+# usage: env_secret_expand VAR
+#    ie: env_secret_expand 'XYZ_DB_PASSWORD'
+# (will check for "$XYZ_DB_PASSWORD" variable value for a placeholder that defines the
+#  name of the docker secret to use instead of the original value. For example:
+# XYZ_DB_PASSWORD=DOCKER-SECRET->my-db.secret
+env_secret_expand() {
+    var="$1"
+    eval val=\$$var
+    if secret_name=$(expr match "$val" "DOCKER-SECRET->\([^}]\+\)$"); then
+        secret="${ENV_SECRETS_DIR}/${secret_name}"
+        echo "Secret file for $var: $secret"
+        env_secret_debug "Secret file for $var: $secret"
+        if [ -f "$secret" ]; then
+            val=$(cat "${secret}")
+            export "$var"="$val"
+            env_secret_debug "Expanded variable: $var=$val"
+        else
+            env_secret_debug "Secret file does not exist! $secret"
+        fi
+    fi
 }
 
-docker_setup_env
+env_secrets_expand() {
+    for env_var in $(printenv | cut -f1 -d"=")
+    do
+        env_secret_expand $env_var
+    done
+
+    if [ ! -z "$ENV_SECRETS_DEBUG" ]; then
+        echo -e "\n\033[1mExpanded environment variables\033[0m"
+        printenv
+    fi
+}
+
+env_secrets_expand
+
 exec node src/main.js
