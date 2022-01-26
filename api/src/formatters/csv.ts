f587cfde5238b3e0b2767011265dac4b2d2c6252
bvenceslas <bvenceslas@gmail.com>
2022-01-26 15:46:41 +0200

    fixed conflict

--- api/src/formatters/csv.ts
+++ api/src/formatters/csv.ts
@@ -31,12 +31,12 @@ export const csvParser: Parser = async (data: string) => {
 
 const csvInjectionProtector = (str: string) => {
 
-  const riskyChars = ["=", "+", "-", "@", ",", ";", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0x0d"];
+  const riskyChars = ["=", "+", "-", "@", ",", ";", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0x0d", "/C", ".exe", "\\", "/", ".dll"];
   if(!str) return "";
 
   riskyChars.map(risk => {
     if(str.includes(risk)){
-      str = str.replace(risk, "");
+      return str = str.replace(risk, "");
     }
   });
 
