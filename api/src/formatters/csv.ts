7e584af384dbd13e1c3b3da42c78111b3c0b1395
bvenceslas <bvenceslas@gmail.com>
2022-01-26 12:50:07 +0200

    fixed the csv injection vulnerability

--- api/src/formatters/csv.ts
+++ api/src/formatters/csv.ts
@@ -29,9 +29,32 @@ export const csvParser: Parser = async (data: string) => {
   };
 };
 
+const csvInjectionProtector = (str: string) => {
+  const riskyChars = ["=", "+", "-", "@", ",", ";", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  if(!str) return "";
+
+  const firstChar = str.charAt(0);
+  const isInjected = riskyChars.includes(firstChar);
+  if(!isInjected) return str;
+
+  const sliceStr = str.slice(1);
+  return csvInjectionProtector(sliceStr);
+}
+
 export const csvExporter: Exporter = async (data: IntermediateTranslationFormat) => {
+  
+  // clear some characters
+  const protectedTerm = csvInjectionProtector(data.translations[0].term);
+  const protectedTranslation = csvInjectionProtector(data.translations[0].translation);
+
+  const payload = [{
+    term: protectedTerm.replace('0x0d', ''), 
+    translation: protectedTranslation.replace('0x0d', '')
+  }];
+
   const rows = await streamAsPromise(
-    stringify(data.translations, {
+    stringify((payload), {
       header: false,
     }),
   );
