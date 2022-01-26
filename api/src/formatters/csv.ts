efa607d94ecfeb70b52deb761df30b60d7779c54
bvenceslas <bvenceslas@gmail.com>
2022-01-26 16:24:17 +0200

    removed the console.log

--- api/src/formatters/csv.ts
+++ api/src/formatters/csv.ts
@@ -53,7 +53,6 @@ export const csvExporter: Exporter = async (data: IntermediateTranslationFormat)
     }
     clearedTranslations.push(protectedTranslation);
   });
-
   const rows = await streamAsPromise(
     stringify((clearedTranslations), {
       header: false,
