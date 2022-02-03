435e6b3bbac172fd737936093d6dac4a5f3bc5db
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-02-03 14:13:09 +0530

    fix: refactor database migration

--- api/src/migrations/1540062777613-add-project-plans.ts
+++ api/src/migrations/1540062777613-add-project-plans.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class addProjectPlans1540062777613 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
-      'CREATE TABLE `plan` (`code` varchar(255) NOT NULL, `name` varchar(255) NOT NULL, `maxStrings` int NOT NULL, `dateCreated` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `dateModified` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), PRIMARY KEY (`code`)) ENGINE=InnoDB',
+      'CREATE TABLE IF NOT EXISTS `plan` (`code` varchar(255) NOT NULL, `name` varchar(255) NOT NULL, `maxStrings` int NOT NULL, `dateCreated` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `dateModified` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), PRIMARY KEY (`code`)) ENGINE=InnoDB',
     );
     await queryRunner.query('ALTER TABLE `project` ADD `termsCount` int NOT NULL DEFAULT 0');
     await queryRunner.query('ALTER TABLE `project` ADD `localesCount` int NOT NULL DEFAULT 0');
@@ -22,6 +22,6 @@ export class addProjectPlans1540062777613 implements MigrationInterface {
     await queryRunner.query('ALTER TABLE `project` DROP COLUMN `planCode`');
     await queryRunner.query('ALTER TABLE `project` DROP COLUMN `localesCount`');
     await queryRunner.query('ALTER TABLE `project` DROP COLUMN `termsCount`');
-    await queryRunner.query('DROP TABLE `plan`');
+    await queryRunner.query('DROP TABLE IF EXISTS `plan`');
   }
 }
--- api/src/migrations/1575719158140-add-label.ts
+++ api/src/migrations/1575719158140-add-label.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class addLabel1575719158140 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
-      'CREATE TABLE `label` (`id` varchar(255) NOT NULL, `value` varchar(255) NOT NULL, `color` varchar(7) NOT NULL, `projectId` varchar(255) NOT NULL, UNIQUE INDEX `IDX_project_terms` (`projectId`, `value`), PRIMARY KEY (`id`)) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin',
+      'CREATE TABLE IF NOT EXISTS `label` (`id` varchar(255) NOT NULL, `value` varchar(255) NOT NULL, `color` varchar(7) NOT NULL, `projectId` varchar(255) NOT NULL, UNIQUE INDEX `IDX_project_terms` (`projectId`, `value`), PRIMARY KEY (`id`)) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin',
     );
     await queryRunner.query(
       'ALTER TABLE `label` ADD CONSTRAINT `FK_project` FOREIGN KEY (`projectId`) REFERENCES `project`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION',
@@ -18,6 +18,6 @@ export class addLabel1575719158140 implements MigrationInterface {
     await queryRunner.query('ALTER TABLE `label` DROP FOREIGN KEY `FK_5ed9c8937635b255539d31b2cce`');
     await queryRunner.query('DROP INDEX `IDX_171dadf4b0b751badd68de0bd3` ON `label`');
     await queryRunner.query('ALTER TABLE `label` DROP FOREIGN KEY `FK_project`');
-    await queryRunner.query('DROP TABLE `label`');
+    await queryRunner.query('DROP TABLE IF EXISTS `label`');
   }
 }
