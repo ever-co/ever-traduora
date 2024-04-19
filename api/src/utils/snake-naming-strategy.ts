c2dbfaad762c75770d6b37e5d228c3833abe4f2c
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-04-19 20:04:10 +0530

    chore(deps): bump @nestjs/core from ^8.0.6 to ^10.3.8

--- api/src/utils/snake-naming-strategy.ts
+++ api/src/utils/snake-naming-strategy.ts
@@ -2,35 +2,35 @@ import { DefaultNamingStrategy, NamingStrategyInterface } from 'typeorm';
 import { snakeCase } from 'typeorm/util/StringUtils';
 
 export class SnakeNamingStrategy extends DefaultNamingStrategy implements NamingStrategyInterface {
-  tableName(className: string, customName: string): string {
-    return customName ? customName : snakeCase(className);
-  }
+	tableName(className: string, customName: string): string {
+		return customName ? customName : snakeCase(className);
+	}
 
-  columnName(propertyName: string, customName: string, embeddedPrefixes: string[]): string {
-    return snakeCase(embeddedPrefixes.concat('').join('_')) + (customName ? customName : snakeCase(propertyName));
-  }
+	columnName(propertyName: string, customName: string, embeddedPrefixes: string[]): string {
+		return snakeCase(embeddedPrefixes.concat('').join('_')) + (customName ? customName : snakeCase(propertyName));
+	}
 
-  relationName(propertyName: string): string {
-    return snakeCase(propertyName);
-  }
+	relationName(propertyName: string): string {
+		return snakeCase(propertyName);
+	}
 
-  joinColumnName(relationName: string, referencedColumnName: string): string {
-    return snakeCase(`${relationName}_${referencedColumnName}`);
-  }
+	joinColumnName(relationName: string, referencedColumnName: string): string {
+		return snakeCase(`${relationName}_${referencedColumnName}`);
+	}
 
-  joinTableName(firstTableName: string, secondTableName: string, firstPropertyName: string, secondPropertyName: string): string {
-    return snakeCase(`${firstTableName}_${firstPropertyName.replace(/\./gi, '_')}_${secondTableName}`);
-  }
+	joinTableName(firstTableName: string, secondTableName: string, firstPropertyName: string, secondPropertyName: string): string {
+		return snakeCase(`${firstTableName}_${firstPropertyName.replace(/\./gi, '_')}_${secondTableName}`);
+	}
 
-  joinTableColumnName(tableName: string, propertyName: string, columnName?: string): string {
-    return snakeCase(`${tableName}_${columnName ? columnName : propertyName}`);
-  }
+	joinTableColumnName(tableName: string, propertyName: string, columnName?: string): string {
+		return snakeCase(`${tableName}_${columnName ? columnName : propertyName}`);
+	}
 
-  classTableInheritanceParentColumnName(parentTableName: any, parentTableIdPropertyName: any): string {
-    return snakeCase(`${parentTableName}_${parentTableIdPropertyName}`);
-  }
+	classTableInheritanceParentColumnName(parentTableName: any, parentTableIdPropertyName: any): string {
+		return snakeCase(`${parentTableName}_${parentTableIdPropertyName}`);
+	}
 
-  eagerJoinRelationAlias(alias: string, propertyPath: string): string {
-    return alias + '__' + propertyPath.replace('.', '_');
-  }
+	eagerJoinRelationAlias(alias: string, propertyPath: string): string {
+		return alias + '__' + propertyPath.replace('.', '_');
+	}
 }
