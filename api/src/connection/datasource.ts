b09e9b2b61e40ae628fa39d22f6daa1db929eab1
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-04-20 14:59:22 +0530

    fix: run prettier for formatting

--- api/src/connection/datasource.ts
+++ api/src/connection/datasource.ts
@@ -9,21 +9,21 @@ const env = process.env;
  * @returns The connection options for TypeORM DataSource or TypeORMModule.
  */
 export const dataSourceOptions = (): DataSourceOptions => {
-    const options: DataSourceOptions = {
-        type: (env.TR_DB_TYPE as any) || 'mysql',
-        host: env.TR_DB_HOST || 'localhost',
-        port: parseInt(env.TR_DB_PORT, 10) || 3306,
-        username: env.TR_DB_USER || 'root',
-        password: env.TR_DB_PASSWORD || 'root',
-        database: env.TR_DB_DATABASE || 'tr_dev',
-        charset: 'utf8mb4',
-        synchronize: false,
-        logging: false,
-        entities: ['src/entity/*.entity*'],
-        migrations: ['src/migrations/*'],
-        namingStrategy: env.TR_DB_TYPE === 'postgres' ? new SnakeNamingStrategy() : new DefaultNamingStrategy()
-    };
-    return options;
+  const options: DataSourceOptions = {
+    type: (env.TR_DB_TYPE as any) || 'mysql',
+    host: env.TR_DB_HOST || 'localhost',
+    port: parseInt(env.TR_DB_PORT, 10) || 3306,
+    username: env.TR_DB_USER || 'root',
+    password: env.TR_DB_PASSWORD || 'root',
+    database: env.TR_DB_DATABASE || 'tr_dev',
+    charset: 'utf8mb4',
+    synchronize: false,
+    logging: false,
+    entities: ['src/entity/*.entity*'],
+    migrations: ['src/migrations/*'],
+    namingStrategy: env.TR_DB_TYPE === 'postgres' ? new SnakeNamingStrategy() : new DefaultNamingStrategy(),
+  };
+  return options;
 };
 
 /**
@@ -31,15 +31,15 @@ export const dataSourceOptions = (): DataSourceOptions => {
  * @returns Initialized TypeORM DataSource instance.
  */
 export const getDataSourceConnection = async (): Promise<DataSource> => {
-    const dataSource = new DataSource(dataSourceOptions());
+  const dataSource = new DataSource(dataSourceOptions());
 
-    try {
-        if (!dataSource.isInitialized) {
-            await dataSource.initialize();
-        }
-    } catch (error) {
-        console.error(error?.message);
+  try {
+    if (!dataSource.isInitialized) {
+      await dataSource.initialize();
     }
+  } catch (error) {
+    console.error(error?.message);
+  }
 
-    return dataSource;
+  return dataSource;
 };
