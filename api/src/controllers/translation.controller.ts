977d9af20ee171cab8106eb85ee303cd046d1e93
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-04-20 14:51:09 +0530

    fix: get data source connection for e2e testing

--- api/src/controllers/translation.controller.ts
+++ api/src/controllers/translation.controller.ts
@@ -32,7 +32,7 @@ export default class TranslationController {
     @InjectRepository(Term) private termRepo: Repository<Term>,
     @InjectRepository(Locale) private localeRepo: Repository<Locale>,
     @InjectRepository(Project) private projectRepo: Repository<Project>,
-  ) { }
+  ) {}
 
   @Get()
   @ApiOperation({ summary: 'List all translation locales for a project' })
@@ -96,7 +96,7 @@ export default class TranslationController {
 
     const result = await this.projectLocaleRepo.findOneByOrFail({
       locale: {
-        code: locale.code
+        code: locale.code,
       },
       project: { id: membership.project.id },
     });
